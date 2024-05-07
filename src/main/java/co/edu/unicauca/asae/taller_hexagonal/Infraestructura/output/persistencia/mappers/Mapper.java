95ad1c7c55b3c8b8366ac00cb9fc1ed673b58f37
FelipePerez99 <andresfppy@gmail.com>
2024-05-07 14:42:23 -0500

    Agregado Mapper para mapeo de objetos

---
+++ src/main/java/co/edu/unicauca/asae/taller_hexagonal/Infraestructura/output/persistencia/mappers/Mapper.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.taller_hexagonal.Infraestructura.output.persistencia.mappers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Mapper {
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+}
